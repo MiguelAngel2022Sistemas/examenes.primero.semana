34436b02d6318fb0a11257fc5946677f3595ac0b
Wilder Julio Espinoza Bravo <wilderespinoza@bcp.com.pe>
2023-06-09 19:49:08 -0500

    primer prueba

---
+++ semana-soap-j19/src/main/java/com/exame/idat/semanasoapj19/SemanaSoapJ19Application.java
@@ -0,0 +1,13 @@
+package com.exame.idat.semanasoapj19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaSoapJ19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaSoapJ19Application.class, args);
+	}
+
+}
---
+++ semana-soap-j19/src/test/java/com/exame/idat/semanasoapj19/SemanaSoapJ19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exame.idat.semanasoapj19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanaSoapJ19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
